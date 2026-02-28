19a81678117c5510baa499df0290014409a70386
SuryaPrabhaKathinokkula <kathinokkulabhavani2006@gmail.com>
2026-02-28 21:44:08 +0530

    Test Class modified

--- src/test/java/com/example/demo/CseControllerTest.java
+++ src/test/java/com/example/demo/CseControllerTest.java
@@ -1,18 +1,25 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CseControllerTest {
-	@Autowired
-	CseController c;
-	@Test
-	void test() {
-		int result = c.addCSE(2, 3);
-		assertEquals(5, result);
-	}
 
+    @Autowired
+    CseController c;
+
+    @Test
+    void testAddCSE() {
+
+        String result = c.addCSE(2, 3);
+
+        assertEquals(
+            "After performing addition, the output is 5. My roll number is 23MH1A0524.",
+            result
+        );
+    }
 }
