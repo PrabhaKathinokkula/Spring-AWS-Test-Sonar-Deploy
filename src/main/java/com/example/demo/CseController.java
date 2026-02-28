6f6f1cb43071808e8fde45a471c9d23841e6eae5
SuryaPrabhaKathinokkula <kathinokkulabhavani2006@gmail.com>
2026-02-28 21:37:43 +0530

    Error modified in CSEController

--- src/main/java/com/example/demo/CseController.java
+++ src/main/java/com/example/demo/CseController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CseController {
-	@GetMapping("/cseadd")
-	public int addCSE(@RequestParam int a, @RequestParam int b) {
-		return a + b;
-		 return "After performing the addition, the final output is " 
-         + result + ". My roll number is 23MH1A05N8.";
-	}
+
+    @GetMapping("/cseadd")
+    public String addCSE(@RequestParam int a,
+                         @RequestParam int b) {
+
+        int result = a + b;
+
+        return "After performing addition, the output is "
+                + result + ". My roll number is 23MH1A0524.";
+    }
 }
